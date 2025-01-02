396d70a48640a4b0b4f882ce5a3ba3586731f50f
0x46641766F7572 <96478305+favourolaoye@users.noreply.github.com>
2025-01-02 21:05:11 +0100

    user io

--- BOOKING APP/main.go
+++ BOOKING APP/main.go
@@ -5,25 +5,31 @@ import "fmt"
 func main()  {
 	// variables
 	var userName string
-	// var totalTickets = 18/
-	var ticketsLeft:uint = 18
+	var ticketsLeft uint
 	var userTickets uint
 
+	ticketsLeft = 18
+
 	fmt.Println("Who uses a cli Car booking app?")
-	fmt.Println("                                 ")
-	// user variables
-	userName = "favour"
-	userTickets = 2
+	fmt.Println("                               ")
+	// user provided values.
+	fmt.Println("Enter nickname: ");
+	fmt.Scan(&userName);
+
 	fmt.Printf("Welcome onboard %v!\n", userName)
 	fmt.Printf("So %v we have %v tickets left\n", userName, ticketsLeft)
+
+	fmt.Println("Enter number of tickets: ");
+	fmt.Scan(&userTickets);
+	
 	fmt.Printf("%v you just booked %v tickets!\n", userName, userTickets)
 	// ticketsLeft = ticketsLeft - userTickets
 
 }
-
+  
 // func Ticketleft(totalTickets, ticketsLeft, userTickets){
 // 	totalTickets = ticketsLeft - userTickets
 // 	ticketsLeft = totalTickets
 
 // 	return ticketsLeft
-// }
+// } n kjllkk
