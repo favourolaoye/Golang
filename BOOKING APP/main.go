5e659aa143041d24fde977a14b9b2a8c2747ad83
0x46641766F7572 <96478305+favourolaoye@users.noreply.github.com>
2025-01-07 03:51:41 +0100

    loops

--- BOOKING APP/main.go
+++ BOOKING APP/main.go
@@ -2,37 +2,37 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
 	// variables
 	var userName string
 	var ticketsLeft uint
 	var userTickets uint
-
+	var bookings []string
 	ticketsLeft = 18
 
 	fmt.Println("Who uses a cli Car booking app?")
 	fmt.Println("                               ")
+
+	for {
+		fmt.Println("Enter nickname: ")
+		fmt.Scan(&userName)
+		bookings = append(bookings, userName)
+		fmt.Printf("Welcome onboard %v!\n", userName)
+		fmt.Printf("So %v we have %v tickets left\n", userName, ticketsLeft)
+
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
+
+		fmt.Printf("%v you just booked %v tickets!\n", userName, userTickets)
+
+		ticketsLeft = ticketsLeft - userTickets
+		fmt.Println("users: ", bookings)
+		fmt.Printf("%v tickets remaining to be booked!\n", ticketsLeft)
+
+		if ticketsLeft == 0 {
+			break
+		}
+	}
 	// user provided values.
-	fmt.Println("Enter nickname: ");
-	fmt.Scan(&userName);
-
-	fmt.Printf("Welcome onboard %v!\n", userName)
-	fmt.Printf("So %v we have %v tickets left\n", userName, ticketsLeft)
-
-	fmt.Println("Enter number of tickets: ");
-	fmt.Scan(&userTickets);
-	
-	fmt.Printf("%v you just booked %v tickets!\n", userName, userTickets)
-
-	ticketsLeft = ticketsLeft - userTickets
-
-	fmt.Printf("%v tickets remaining to be booked!\n", ticketsLeft)
 
 }
-  
-// func Ticketleft(totalTickets, ticketsLeft, userTickets){
-// 	totalTickets = ticketsLeft - userTickets
-// 	ticketsLeft = totalTickets
-
-// 	return ticketsLeft
-// } n kjllkk
